799083071ccf4b32d5f5e26d3f3b54019a9c884e
mmarciosantos <marcio-santos2001@hotmail.com>
2022-10-19 11:47:49 -0300

    Adicionado comentários no script que remove usuários, grupos e diretórios

--- os-linux/remove_usuarios.sh
+++ os-linux/remove_usuarios.sh
@@ -1,15 +1,18 @@
 #!/bin/env bash
 
+#Remove todos os usuários
 for i in $(cat cria_usuarios.sh | grep useradd | cut -d " " -f2)
 do
 	userdel -r -f $i
 done
 
+#Remove todos os grupos
 for i in $(cat cria_usuarios.sh | grep groupadd | cut -d " " -f2)
 do
 	groupdel $i
 done
 
+#Remove todos os diretórios
 for i in $(cat cria_usuarios.sh | grep mkdir | cut -d " " -f2)
 do
 	rm -rf $i
